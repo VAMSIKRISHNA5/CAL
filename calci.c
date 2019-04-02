2efe57e4cac38d0d348a7dd6054a2bbe463e37dd
VAMSIKRISHNA5 <42820576+VAMSIKRISHNA5@users.noreply.github.com>
2019-04-02 08:40:12 +0530

    Update calci.c

--- calci.c
+++ calci.c
@@ -4,19 +4,19 @@
 #include "calculat.h"
 void testcase(void)
 {
-    assert(calc_1(10,9,1)== 19);
+    assert(calc(10,9,1)== 19);
  printf("\nOK");
-  assert(calc_1(11,-11,1)== 0);
+  assert(calc(11,-11,1)== 0);
  printf("\nOK");
- assert(calc_1(-13,-3,1)== -16);
+ assert(calc(-13,-3,1)== -16);
  printf("\nOK");
- assert(calc_1(15,10,2)== 5);
+ assert(calc(15,10,2)== 5);
  printf("\nOK");
- assert(calc_1(16,-3,2)== 19);
+ assert(calc(16,-3,2)== 19);
  printf("\nOK");
- assert(calc_1(-2,-48,2)== 46);
+ assert(calc(-2,-48,2)== 46);
  printf("\nOK");
- assert(calc_1(32,0,4)== 0);
+ assert(calc(32,0,4)== 0);
  printf("\nOK");
 }
 void main(void){
